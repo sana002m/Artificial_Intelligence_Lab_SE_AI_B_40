2525d0913703243243e20058faac5d7ce208efca
sana002m <sana.maulavi24@mmit.edu.in>
2025-08-05 12:01:54 +0530

    FILE IS CREATED

---
+++ Artificial_Intelligence_Lab/assignment2a.py
@@ -0,0 +1,21 @@
+subject1=input("ENTER SUBJECT : ")
+subject2=(input("ENTER ANOTHER SUBJECT : "))
+if(subject1 == "Programming" and subject2 == "Statistics"):
+         print("BRANCH : ARTIFICIAL INTELLIGENCE AND DATA SCIENCE")
+         
+elif(subject1 == "Physics" and subject2 == "Maths"):       
+         print("BRANCH : MECHANICAL ENGINEERING")
+         
+elif(subject1 == "Programming" and subject2 == "Maths"):
+         print("BRANCH : COMPUTER ENGINEERING")
+         
+elif(subject1 == "Biology" and subject2 == "Chemistry"):
+         print("BRANCH : BIOTECHNOLOGY")
+         
+elif(subject1 == "Circuits" and subject2 == "Maths"):
+         print("BRANCH : ELECTRONICS ENGINEERING")
+         
+elif(subject1 == "Programming" and subject2 == "AIConcepts"):
+         print("BRANCH : ARTIFICIAL INTELLIGENCE AND MACHINE LEARNING")
+         
+
